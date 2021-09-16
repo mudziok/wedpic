2459cfb5e096824868c557b0909de3185f2382e8
mudziok <mudziok@gmail.com>
2021-09-16 21:44:52 +0200

    Update ThankYouModal UI

--- src/preview/thankYouModal.tsx
+++ src/preview/thankYouModal.tsx
@@ -1,7 +1,6 @@
 import { FC } from "react";
 import { Modal } from "../modal/modal";
 import { ButtonWithIcon } from "../utility/buttonWithIcon";
-import { iconCamera } from "../utility/icons";
 
 interface thankYouModalProps {
   onExit: () => void,
@@ -12,13 +11,14 @@ export const ThankYouModal:FC<thankYouModalProps> = ({onExit, onClickOut}) => {
   return (
     <Modal
       onClickOut={onClickOut}>
-      <div className='flex flex-col justify-center items-center'>
+      <div className='flex flex-col justify-center items-center space-y-5'>
         <span className="text-xl p-2">Dziękujemy za wysłanie zdjęcia!</span>
-        <ButtonWithIcon
-          text={"Zrób kolejne zdjęcie"}
-          icon={iconCamera}
-          onClick={onExit}
-        />
+        <div className="border rounded-xl">
+          <ButtonWithIcon
+            text={"Zrób kolejne zdjęcie"}
+            onClick={onExit}
+          />
+        </div>
       </div>
     </Modal>
   )
