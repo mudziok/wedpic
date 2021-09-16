c313069a4559405d075feb6eda78094c3b39be6e
mudziok <mudziok@gmail.com>
2021-09-16 13:23:02 +0200

    Create and implement Camera Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import { FC, useState } from 'react';
 import './App.css';
+import { Camera } from './camera/camera';
 
-function App() {
+const App:FC = () => {
+  const [imgFile, setImgFile] = useState<File | null>(null);
+  // eslint-disable-next-line
+  const [isTakingPhoto, setIsTakingPhoto] = useState<boolean>(true);
+
+  const processFile = (f: File) => {
+    setImgFile(f)
+    console.log(imgFile, f)
+  }
+
   return (
-    <div className="App">
+    <div className="w-screen h-screen bg-black">
+      <Camera
+        onFileSelected={processFile}
+      />
     </div>
   );
 }
---
+++ src/camera/camera.tsx
@@ -0,0 +1,64 @@
+import { useRef, useState, useCallback, ChangeEvent, FC } from "react";
+import Webcam from "react-webcam";
+import { Overlay } from "./overlay";
+import { urlToFile } from "../utility/urlToFile";
+
+interface cameraProps {
+  onFileSelected: (f: File) => void
+}
+
+export const Camera:FC<cameraProps> = ({onFileSelected}) => {
+  const webcamRef = useRef<Webcam>(null);
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const [isFrontFacing, setFrontFacing] = useState(true);
+
+  const flipFacingMode = () => {
+    setFrontFacing(!isFrontFacing);
+  }
+
+  const capture = useCallback(async () => {
+    if (webcamRef.current) {
+      const screenSrc = webcamRef.current.getScreenshot();
+      if (screenSrc) {
+        const screenFile = await urlToFile(screenSrc, "photo.jpeg", 'image/jpeg')
+        onFileSelected(screenFile);
+      }
+    }
+  }, [webcamRef, onFileSelected]);
+
+  const openFileSelector = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click()
+    }
+  }
+
+  const uploadLocal = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.currentTarget.files) {
+      const screenFile = event.currentTarget.files[0]
+      onFileSelected(screenFile);
+    }
+  }
+
+  return (
+    <div className="w-full h-full flex justify-center items-center">
+      <Webcam
+        ref={webcamRef}
+        screenshotFormat="image/jpeg"
+        mirrored={isFrontFacing}
+        videoConstraints={{facingMode: isFrontFacing ? "user" : { exact: "environment" }}}
+      />
+      <div className="fixed top-0 left-0 w-screen h-screen">
+        <input className="hidden"
+          type="file" ref={fileInputRef}
+          accept="image/png, image/jpg, image/jpeg, .heic"
+          onChange={uploadLocal}
+        />
+        <Overlay
+          onSwitchFacing={flipFacingMode}
+          onCapturePhoto={capture}
+          onUploadPhoto={openFileSelector}
+        />
+      </div>
+    </div>
+    )
+}
